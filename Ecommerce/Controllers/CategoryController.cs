921782ed8b583669418450f7fc613146eb8daf9d
YousefBelal <yousefbelal265@gmail.com>
2025-07-26 11:18:37 +0200

    Added Categories API

---
+++ Ecommerce/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using Ecommerce.Infrastructure.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoryController : ControllerBase
+    {
+        public readonly ICategoryService categoryService;
+        public CategoryController(ICategoryService categoryService)
+        {
+            this.categoryService = categoryService;
+        }
+
+        [HttpGet("GetCategories")]
+        public IActionResult GetCategories()
+        {
+            return Ok(categoryService.GetCategories());
+        }
+    }
+}
---
+++ Ecommerce/Infrastructure/Services/CategoryService.cs
@@ -0,0 +1,19 @@
+﻿using Ecommerce.Infrastructure.Context;
+using Ecommerce.Infrastructure.Entities;
+using Ecommerce.Infrastructure.Interfaces;
+
+namespace Ecommerce.Infrastructure.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        internal readonly EcommerceDbContext _context;
+        public CategoryService(EcommerceDbContext context)
+        {
+            _context = context;
+        }
+        public List<Categories> GetCategories()
+        {
+            return _context.Categories.ToList();
+        }
+    }
+}
